2a5a44493e4081fe9545440dc16e0efa3b2ece4d
Benjamin Waife <bwaife@gmail.com>
2025-07-20 09:15:17 +0100

    edit main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+
+# AWS Provider Configuration for Terraform
+
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  } 
+}
+
+provider "aws" {
+    region = "us-west-2"
+    default_tags {
+      
+    }
+  
+}
